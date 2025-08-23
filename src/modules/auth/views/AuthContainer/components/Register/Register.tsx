e18a444d35d011de024d7373086231ebc7586c49
Huu Tuan Nguyen <huutuan_nguyen@datahouse.com>
2025-08-23 23:23:27 +0700

    feat(auth): implement registration form with validation and submission handling

--- src/modules/auth/views/AuthContainer/components/Register/Register.tsx
+++ src/modules/auth/views/AuthContainer/components/Register/Register.tsx
@@ -1,52 +1,77 @@
 import {
   Button,
   Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
   Input,
-  Label,
 } from "@/components";
+import { useRegisterForm } from "./useRegisterForm";
+import { AuthKeys } from "@/modules/auth/bases";
 
 export const Register = () => {
+  const { form, onSubmit } = useRegisterForm();
+  const { control } = form;
+
   return (
     <Card className="w-full rounded-sm">
-      <CardHeader>
-        <CardTitle>Create a new account</CardTitle>
-        <CardDescription>
+      <Card.Header>
+        <Card.Title>Create a new account</Card.Title>
+        <Card.Description>
           Enter your email and password below to create a new account
-        </CardDescription>
-      </CardHeader>
-      <CardContent>
-        <form>
-          <div className="flex flex-col gap-6">
-            <div className="grid gap-2">
-              <Label htmlFor="email">Email</Label>
-              <Input
-                id="email"
-                type="email"
-                placeholder="m@example.com"
-                required
-              />
-            </div>
-            <div className="grid gap-2">
-              <Label htmlFor="password">Password</Label>
-              <Input id="password" type="password" required />
-            </div>
-            <div className="grid gap-2">
-              <Label htmlFor="confirm-password">Confirm Password</Label>
-              <Input id="confirm-password" type="password" required />
-            </div>
-          </div>
-        </form>
-      </CardContent>
-      <CardFooter className="flex-col gap-2">
-        <Button type="submit" className="w-full">
-          Register
-        </Button>
-      </CardFooter>
+        </Card.Description>
+      </Card.Header>
+      <Card.Content>
+        <Form {...form}>
+          <form noValidate onSubmit={onSubmit} className="space-y-6">
+            <FormField
+              control={control}
+              name={AuthKeys.EMAIL}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Enter your email" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={control}
+              name={AuthKeys.PASSWORD}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input type="password" placeholder="********" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={control}
+              name={AuthKeys.CONFIRM_PASSWORD}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Confirm Password</FormLabel>
+                  <FormControl>
+                    <Input type="password" placeholder="********" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit" className="w-full">
+              Register
+            </Button>
+          </form>
+        </Form>
+      </Card.Content>
     </Card>
   );
 };
