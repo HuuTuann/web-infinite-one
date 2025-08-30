5e6a284d579b0caadb71d9df417c4abab9ea5f6a
Huu Tuan Nguyen <huutuan_nguyen@datahouse.com>
2025-08-30 21:43:51 +0700

    refactor(auth): move RequireAuth component to a shared location and clean up auth module exports

--- src/components/guards/RequireAuth/RequireAuth.tsx
+++ src/components/guards/RequireAuth/RequireAuth.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Toastify } from "@/components/io-ui";
+import { useHydration } from "@/hooks";
 import { useAuthStore } from "@/stores";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
@@ -12,17 +13,25 @@ type Props = {
 
 export const RequireAuth = ({ children, redirectTo = "/auth" }: Props) => {
   const router = useRouter();
+  const isHydrated = useHydration();
   const isAuthenticated = useAuthStore((s) => s.isAuthenticated);
 
   useEffect(() => {
-    if (!isAuthenticated) {
+    if (isHydrated && !isAuthenticated) {
       setTimeout(() => {
         router.replace(redirectTo);
       }, 300);
       Toastify.Error("Unauthorized access");
     }
-  }, [isAuthenticated, redirectTo, router]);
+  }, [isAuthenticated, isHydrated, redirectTo, router]);
 
-  if (!isAuthenticated) return null;
-  return <>{children}</>;
+  if (!isHydrated) {
+    return <div>Loading...</div>;
+  }
+
+  if (isAuthenticated) {
+    return <>{children}</>;
+  }
+
+  return <div>Checking authentication...</div>;
 };
